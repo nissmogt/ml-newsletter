89eb8e042ca0951e92976ae1317432267d407541
nissmogt <k.mehrabiani@gmail.com>
2024-08-09 14:24:14 -0700

    added the main summary generator method

--- generate_newsletter.py
+++ generate_newsletter.py
@@ -1,4 +1,6 @@
 from postprocess import generate_newsletter
+from paper_summary_generator import run_generator
 
 if __name__ == "__main__":
+    run_generator(test=False)
     generate_newsletter()
